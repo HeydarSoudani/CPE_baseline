4a6ac1b319f4c56a4cbf89927b1a3c84fdd25b21
Heydar Soudani <heydar.soudani@gmail.com>
2022-01-01 15:45:10 +0330

    mlp added

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 from math import ceil
+import math
 from sklearn.metrics import accuracy_score, confusion_matrix
 
 compute_distance = nn.PairwiseDistance(p=2, eps=1e-6)
@@ -266,6 +267,16 @@ class Conv_4(nn.Module):
     def load(self, path):
         state_dict = torch.load(path)
         self.load_state_dict(state_dict)
+
+
+def Xavier(m):
+  if m.__class__.__name__ == 'Linear':
+    fan_in, fan_out = m.weight.data.size(1), m.weight.data.size(0)
+    std = 1.0 * math.sqrt(2.0 / (fan_in + fan_out))
+    a = math.sqrt(3.0) * std
+    m.weight.data.uniform_(-a, a)
+    if m.bias is not None:
+      m.bias.data.fill_(0.0)
 
 class MLP(nn.Module):
   def __init__(self, n_input, n_feature, n_output, args, bias=True):
